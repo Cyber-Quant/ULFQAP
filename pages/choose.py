98f73a306dfd601527d87b4bae71f2f72580635c
Jia Liu <proljc@gmail.com>
2020-11-30 19:06:35 +0800

    separate common widget from choose page

--- pages/choose.py
+++ pages/choose.py
@@ -1,28 +1,24 @@
 import json
-import pyqtgraph as pg
 
-from pyqtgraph.dockarea import *
 from qtpy.QtWidgets import *
 from qtpy.QtGui import *
 from qtpy.QtCore import *
 
-from conf.conf import fav_stocks_config_path, apply_strategies_config_path, \
-    DEFAULT_K_LIMIT
+from conf.conf import fav_stocks_config_path, apply_strategies_config_path
 from db.models import AStockInfo
-from strategies.common import get_latest_batch_data
-from strategies.boll import BOLL, BOLLChoose, BOLLInfo
-from strategies.dual_ma import DualMA, DualMAChoose, DualMAInfo
-from strategies.kdj import KDJ, KDJChoose, KDJInfo
-from strategies.macd import MACD, MACDChoose, MACDInfo
-from strategies.rsi import RSI, RSIChoose, RSIInfo
+from strategies.boll import BOLLChoose, BOLLInfo
+from strategies.dual_ma import DualMAChoose, DualMAInfo
+from strategies.kdj import KDJChoose, KDJInfo
+from strategies.macd import MACDChoose, MACDInfo
+from strategies.rsi import RSIChoose, RSIInfo
 from strategies.volume_increase import VolumeIncreaseChoose, VolumeIncreaseInfo
-from strategies.wr import WR, WRChoose, WRInfo
-from utils.candlestick import CandlestickItem
+from strategies.wr import WRChoose, WRInfo
 from utils.custom_add_dialog import CustomAddDialog
 
+from widgets.plots import Plots
 
-# TODO: separate common plot widget from this page and watch page
-class Choose(QWidget):
+
+class Choose(Plots):
     fav_stock_changed_signal = Signal()
     kline_info_signal = Signal(str, float, float, float, float, int)
 
@@ -35,24 +31,6 @@ class Choose(QWidget):
         self.stocks_to_be_chosen = []
         self.stocks_pre_chose = []
         self.apply_strategies = []
-
-        self.kline_data = []
-        self.k_v_line = pg.InfiniteLine(angle=90, movable=False)
-        self.k_h_line = pg.InfiniteLine(angle=0, movable=False)
-        self.vol_v_line = pg.InfiniteLine(angle=90, movable=False)
-        self.vol_h_line = pg.InfiniteLine(angle=0, movable=False)
-        self.macd_v_line = pg.InfiniteLine(angle=90, movable=False)
-        self.macd_h_line = pg.InfiniteLine(angle=0, movable=False)
-        self.kdj_v_line = pg.InfiniteLine(angle=90, movable=False)
-        self.kdj_h_line = pg.InfiniteLine(angle=0, movable=False)
-        self.rsi_v_line = pg.InfiniteLine(angle=90, movable=False)
-        self.rsi_h_line = pg.InfiniteLine(angle=0, movable=False)
-        self.wr_v_line = pg.InfiniteLine(angle=90, movable=False)
-        self.wr_h_line = pg.InfiniteLine(angle=0, movable=False)
-
-        self.current_kline_code = None
-        self.current_indicatrix_name = None
-        self.current_kline_period = 'd'
 
         self.choose_thread = None
         # TODO: make strategies plugin
@@ -165,62 +143,6 @@ class Choose(QWidget):
         info_h_box.addWidget(self.info_widget)
 
         right_v_box = QVBoxLayout()
-        self.plt_area = DockArea()
-
-        self.dock_k = Dock('K')
-        self.k_plt = pg.PlotWidget(enableMenu=False)
-        self.k_plt.plotItem.setMouseEnabled(y=False)
-        self.k_plt.hideAxis('bottom')
-        self.info_label = pg.TextItem()
-        self.plt_area.addDock(self.dock_k, 'bottom')
-        self.dock_k.addWidget(self.k_plt)
-
-        index_height = 70
-
-        self.dock_vol = Dock('Volume')
-        self.vol_plt = pg.PlotWidget(enableMenu=False)
-        self.vol_plt.plotItem.setMouseEnabled(y=False)
-        self.vol_plt.hideAxis('bottom')
-        self.vol_plt.setXLink(self.k_plt)
-        self.plt_area.addDock(self.dock_vol, 'bottom', self.dock_k)
-        self.dock_vol.addWidget(self.vol_plt)
-        self.dock_vol.setFixedHeight(index_height)
-
-        self.dock_macd = Dock('MACD')
-        self.macd_plt = pg.PlotWidget(enableMenu=False)
-        self.macd_plt.plotItem.setMouseEnabled(y=False)
-        self.macd_plt.hideAxis('bottom')
-        self.macd_plt.setXLink(self.k_plt)
-        self.plt_area.addDock(self.dock_macd, 'bottom', self.dock_vol)
-        self.dock_macd.addWidget(self.macd_plt)
-        self.dock_macd.setFixedHeight(index_height)
-
-        self.dock_kdj = Dock('KDJ')
-        self.kdj_plt = pg.PlotWidget(enableMenu=False)
-        self.kdj_plt.plotItem.setMouseEnabled(y=False)
-        self.kdj_plt.hideAxis('bottom')
-        self.kdj_plt.setXLink(self.k_plt)
-        self.plt_area.addDock(self.dock_kdj, 'bottom', self.dock_macd)
-        self.dock_kdj.addWidget(self.kdj_plt)
-        self.dock_kdj.setFixedHeight(index_height)
-
-        self.dock_rsi = Dock('RSI')
-        self.rsi_plt = pg.PlotWidget(enableMenu=False)
-        self.rsi_plt.plotItem.setMouseEnabled(y=False)
-        self.rsi_plt.hideAxis('bottom')
-        self.rsi_plt.setXLink(self.k_plt)
-        self.plt_area.addDock(self.dock_rsi, 'bottom', self.dock_kdj)
-        self.dock_rsi.addWidget(self.rsi_plt)
-        self.dock_rsi.setFixedHeight(index_height)
-
-        self.dock_wr = Dock('WR')
-        self.wr_plt = pg.PlotWidget(enableMenu=False)
-        self.wr_plt.plotItem.setMouseEnabled(y=False)
-        self.wr_plt.hideAxis('bottom')
-        self.wr_plt.setXLink(self.k_plt)
-        self.plt_area.addDock(self.dock_wr, 'bottom', self.dock_rsi)
-        self.dock_wr.addWidget(self.wr_plt)
-        self.dock_wr.setFixedHeight(index_height)
 
         right_v_box.addLayout(info_h_box)
         right_v_box.addWidget(self.plt_area)
@@ -237,25 +159,7 @@ class Choose(QWidget):
         self.btn_choose.clicked.connect(self.on_choose)
         self.btn_stop_choose.clicked.connect(self.on_stop_choose)
         self.table.customContextMenuRequested.connect(self.open_pool_ops_menu)
-        self.k_move_slot = pg.SignalProxy(self.k_plt.scene().sigMouseMoved,
-                                          rateLimit=60,
-                                          slot=self.kline_emit_info)
-        self.vol_move_slot = pg.SignalProxy(self.vol_plt.scene().sigMouseMoved,
-                                            rateLimit=60,
-                                            slot=self.vol_emit_info)
-        self.macd_move_slot = pg.SignalProxy(
-            self.macd_plt.scene().sigMouseMoved,
-            rateLimit=60,
-            slot=self.macd_emit_info)
-        self.kdj_move_slot = pg.SignalProxy(self.kdj_plt.scene().sigMouseMoved,
-                                            rateLimit=60,
-                                            slot=self.kdj_emit_info)
-        self.rsi_move_slot = pg.SignalProxy(self.rsi_plt.scene().sigMouseMoved,
-                                            rateLimit=60,
-                                            slot=self.rsi_emit_info)
-        self.wr_move_slot = pg.SignalProxy(self.wr_plt.scene().sigMouseMoved,
-                                           rateLimit=60,
-                                           slot=self.wr_emit_info)
+
         self.table.itemSelectionChanged.connect(self.on_row_changed)
         self.kline_info_signal.connect(self.on_kline_info_changed)
 
@@ -276,280 +180,6 @@ class Choose(QWidget):
         self.render_all_plots(code)
         self.re_draw_indicatrix(self.current_indicatrix_name)
 
-    def re_render_all_plots(self, code):
-        if code is None:
-            return
-        else:
-            self.current_kline_code = code
-            self.render_all_plots(code)
-
-    def render_all_plots(self, code):
-        self.current_kline_code = code
-        if self.current_kline_code is None:
-            return
-
-        self.kline_data = []
-        date, _open, close, high, low, volume, ma_price, ma_volume = \
-            get_latest_batch_data(code, DEFAULT_K_LIMIT,
-                                  period=self.current_kline_period)
-
-        _macd = MACD()
-        macd, dif, dea = _macd.calc_macd(close)
-
-        _kdj = KDJ()
-        k, d, j = _kdj.calc_kdj(close, high, low)
-
-        _rsi = RSI()
-        fast_rsi, slow_rsi = _rsi.calc_rsi(close)
-
-        _wr = WR()
-        wr = _wr.calc_williams(close, high, low)
-
-        for i in range(len(close)):
-            obj = {
-                'id': i,
-                'date': date[i],
-                'code': code,
-                'open': _open[i],
-                'close': close[i],
-                'high': high[i],
-                'low': low[i],
-                'volume': volume[i],
-                'dif': dif[i],
-                'dea': dea[i],
-                'macd': macd[i],
-                'k': k[i],
-                'd': d[i],
-                'j': j[i],
-                'slow_rsi': slow_rsi[i],
-                'fast_rsi': fast_rsi[i],
-                'wr': wr[i]
-            }
-            self.kline_data.append(obj)
-        self._render_all_plots()
-
-    def _render_all_plots(self):
-        self.k_plt.plotItem.clear()
-        uni_index = []
-
-        k_data = []
-        lows = []
-        highs = []
-        k_axis = []
-
-        volumes = []
-
-        difs = []
-        deas = []
-        red_macds = []
-        green_macds = []
-        red_macds_index = []
-        green_macds_index = []
-
-        k = []
-        d = []
-        j = []
-
-        slow_rsis = []
-        fast_rsis = []
-
-        wrs = []
-
-        for item in self.kline_data:
-            uni_index.append(item['id'])
-
-            (i, _open, close, high, low) = (
-                item['id'], item['open'], item['close'], item['high'],
-                item['low'])
-            k_data.append((i, _open, close, high, low))
-            lows.append(item['low'])
-            highs.append(item['high'])
-            k_axis.append(item['date'])
-
-            volumes.append(item['volume'])
-
-            difs.append(item['dif'])
-            deas.append(item['dea'])
-            if item['macd'] >= 0:
-                red_macds.append(item['macd'])
-                red_macds_index.append(item['id'])
-            else:
-                green_macds.append(item['macd'])
-                green_macds_index.append(item['id'])
-
-            k.append(item['k'])
-            d.append(item['d'])
-            j.append(item['j'])
-
-            slow_rsis.append(item['slow_rsi'])
-            fast_rsis.append(item['fast_rsi'])
-
-            wrs.append(item['wr'])
-
-        axis = zip(range(len(k_axis)), k_axis)
-        item = CandlestickItem(k_data)
-        y_min = min(lows)
-        y_max = max(highs)
-        self.k_plt.getAxis('bottom').setTicks([axis])
-        self.k_plt.addItem(item)
-        self.k_plt.showGrid(True, True)
-        self.k_plt.setYRange(y_min, y_max)
-        self.k_plt.addItem(self.k_v_line, ignoreBounds=True)
-        self.k_plt.addItem(self.k_h_line, ignoreBounds=True)
-        self.k_plt.addItem(self.info_label)
-
-        uni_width = (k_data[1][0] - k_data[0][0]) / 3.0
-
-        self.vol_plt.plotItem.clear()
-        volume_bar = pg.BarGraphItem(x=uni_index, height=volumes,
-                                     width=uni_width,
-                                     pen='b')
-        self.vol_plt.addItem(volume_bar)
-        self.vol_plt.addItem(self.vol_v_line, ignoreBounds=True)
-        self.vol_plt.addItem(self.vol_h_line, ignoreBounds=True)
-
-        self.macd_plt.plotItem.clear()
-        red_macd_bar = pg.BarGraphItem(x=red_macds_index, height=red_macds,
-                                       width=uni_width,
-                                       pen='r')
-        green_macd_bar = pg.BarGraphItem(x=green_macds_index,
-                                         height=green_macds,
-                                         width=uni_width,
-                                         pen='g')
-        self.macd_plt.addItem(red_macd_bar)
-        self.macd_plt.addItem(green_macd_bar)
-        self.macd_plt.plot(difs, pen='w')
-        self.macd_plt.plot(deas, pen='y')
-        self.macd_plt.addItem(self.macd_v_line, ignoreBounds=True)
-        self.macd_plt.addItem(self.macd_h_line, ignoreBounds=True)
-
-        self.kdj_plt.plotItem.clear()
-        self.kdj_plt.plot(k, pen='r')
-        self.kdj_plt.plot(d, pen='b')
-        self.kdj_plt.plot(j, pen='y')
-        self.kdj_plt.addItem(self.kdj_v_line, ignoreBounds=True)
-        self.kdj_plt.addItem(self.kdj_h_line, ignoreBounds=True)
-
-        self.rsi_plt.plotItem.clear()
-        self.rsi_plt.plot(slow_rsis, pen='b')
-        self.rsi_plt.plot(fast_rsis, pen='y')
-        self.rsi_plt.addItem(self.rsi_v_line, ignoreBounds=True)
-        self.rsi_plt.addItem(self.rsi_h_line, ignoreBounds=True)
-
-        self.wr_plt.plotItem.clear()
-        self.wr_plt.plot(wrs, pen='w')
-        self.wr_plt.addItem(self.wr_v_line, ignoreBounds=True)
-        self.wr_plt.addItem(self.wr_h_line, ignoreBounds=True)
-
-    def _emit_info(self, mouse_point):
-        index = int(mouse_point.x())
-        if -1 < index < len(self.kline_data):
-            volume = int(self.kline_data[index]['volume'] / 100)
-            self.info_label.setHtml(
-                "<p style='color:white'><strong>日期：{0}</strong></p>"
-                "<p style='color:white'>开：{1}</p>"
-                "<p style='color:white'>高：{2}</p>"
-                "<p style='color:white'>低：{3}</p>"
-                "<p style='color:white'>收：{4}</p>"
-                "<p style='color:white'>量：{5}</p>".format(
-                    self.kline_data[index]['date'],
-                    self.kline_data[index]['open'],
-                    self.kline_data[index]['high'],
-                    self.kline_data[index]['low'],
-                    self.kline_data[index]['close'],
-                    volume))
-            self.info_label.setPos(mouse_point.x(), mouse_point.y())
-            self.kline_info_signal.emit(self.kline_data[index]['date'],
-                                        self.kline_data[index]['open'],
-                                        self.kline_data[index]['close'],
-                                        self.kline_data[index]['high'],
-                                        self.kline_data[index]['low'],
-                                        volume)
-
-    def kline_emit_info(self, event):
-        pos = event[0]
-        if self.k_plt.sceneBoundingRect().contains(pos):
-            mouse_point = self.k_plt.plotItem.vb.mapSceneToView(pos)
-            self._emit_info(mouse_point)
-
-            self.k_v_line.setPos(mouse_point.x())
-            self.k_h_line.setPos(mouse_point.y())
-            self.vol_v_line.setPos(mouse_point.x())
-            self.macd_v_line.setPos(mouse_point.x())
-            self.kdj_v_line.setPos(mouse_point.x())
-            self.rsi_v_line.setPos(mouse_point.x())
-            self.wr_v_line.setPos(mouse_point.x())
-
-    def vol_emit_info(self, event):
-        pos = event[0]
-        if self.vol_plt.sceneBoundingRect().contains(pos):
-            mouse_point = self.vol_plt.plotItem.vb.mapSceneToView(pos)
-            self._emit_info(mouse_point)
-
-            self.vol_v_line.setPos(mouse_point.x())
-            self.vol_h_line.setPos(mouse_point.y())
-            self.k_v_line.setPos(mouse_point.x())
-            self.macd_v_line.setPos(mouse_point.x())
-            self.kdj_v_line.setPos(mouse_point.x())
-            self.rsi_v_line.setPos(mouse_point.x())
-            self.wr_v_line.setPos(mouse_point.x())
-
-    def macd_emit_info(self, event):
-        pos = event[0]
-        if self.macd_plt.sceneBoundingRect().contains(pos):
-            mouse_point = self.macd_plt.plotItem.vb.mapSceneToView(pos)
-            self._emit_info(mouse_point)
-
-            self.macd_v_line.setPos(mouse_point.x())
-            self.macd_h_line.setPos(mouse_point.y())
-            self.k_v_line.setPos(mouse_point.x())
-            self.vol_v_line.setPos(mouse_point.x())
-            self.kdj_v_line.setPos(mouse_point.x())
-            self.rsi_v_line.setPos(mouse_point.x())
-            self.wr_v_line.setPos(mouse_point.x())
-
-    def kdj_emit_info(self, event):
-        pos = event[0]
-        if self.kdj_plt.sceneBoundingRect().contains(pos):
-            mouse_point = self.kdj_plt.plotItem.vb.mapSceneToView(pos)
-            self._emit_info(mouse_point)
-
-            self.kdj_v_line.setPos(mouse_point.x())
-            self.kdj_h_line.setPos(mouse_point.y())
-            self.k_v_line.setPos(mouse_point.x())
-            self.vol_v_line.setPos(mouse_point.x())
-            self.macd_v_line.setPos(mouse_point.x())
-            self.rsi_v_line.setPos(mouse_point.x())
-            self.wr_v_line.setPos(mouse_point.x())
-
-    def rsi_emit_info(self, event):
-        pos = event[0]
-        if self.rsi_plt.sceneBoundingRect().contains(pos):
-            mouse_point = self.rsi_plt.plotItem.vb.mapSceneToView(pos)
-            self._emit_info(mouse_point)
-
-            self.rsi_v_line.setPos(mouse_point.x())
-            self.rsi_h_line.setPos(mouse_point.y())
-            self.k_v_line.setPos(mouse_point.x())
-            self.vol_v_line.setPos(mouse_point.x())
-            self.macd_v_line.setPos(mouse_point.x())
-            self.kdj_v_line.setPos(mouse_point.x())
-            self.wr_v_line.setPos(mouse_point.x())
-
-    def wr_emit_info(self, event):
-        pos = event[0]
-        if self.wr_plt.sceneBoundingRect().contains(pos):
-            mouse_point = self.wr_plt.plotItem.vb.mapSceneToView(pos)
-            self._emit_info(mouse_point)
-
-            self.wr_v_line.setPos(mouse_point.x())
-            self.wr_h_line.setPos(mouse_point.y())
-            self.k_v_line.setPos(mouse_point.x())
-            self.vol_v_line.setPos(mouse_point.x())
-            self.macd_v_line.setPos(mouse_point.x())
-            self.kdj_v_line.setPos(mouse_point.x())
-            self.rsi_v_line.setPos(mouse_point.x())
-
     def on_kline_info_changed(self, date, _open, close, high, low, volume):
         self.date_input.setText(date)
         self.open_input.setText(str(_open))
@@ -557,46 +187,6 @@ class Choose(QWidget):
         self.high_input.setText(str(high))
         self.low_input.setText(str(low))
         self.volume_input.setText(str(volume))
-
-    def re_draw_indicatrix(self, name):
-        self.current_indicatrix_name = name
-        if self.current_kline_code is None:
-            return
-        self.draw_indicatrix(name)
-
-    def draw_indicatrix(self, strategy_name):
-        self.current_indicatrix_name = strategy_name
-        if self.current_kline_code is None or \
-                self.current_indicatrix_name is None:
-            return
-
-        # NEW STRATEGIES #
-        if self.current_indicatrix_name == self.dual_ma_info.name:
-            dual_ma = DualMA()
-            short_period_mas = dual_ma.calc_short_period_ma(
-                self.current_kline_code)
-            long_period_mas = dual_ma.calc_long_period_ma(
-                self.current_kline_code)
-            data = [short_period_mas, long_period_mas]
-            pen_colors = ['r', 'b']
-        elif self.current_indicatrix_name == self.boll_info.name:
-            boll = BOLL()
-            ups = boll.calc_batch_up(self.current_kline_code)
-            middles = boll.calc_batch_middle(self.current_kline_code)
-            downs = boll.calc_batch_down(self.current_kline_code)
-            data = [ups, middles, downs]
-            pen_colors = ['r', 'w', 'b']
-        else:
-            data = []
-            pen_colors = []
-        self.draw_lines(data, pen_colors)
-
-    def draw_lines(self, data, pen_colors):
-        for i, _data in enumerate(data):
-            self._draw_line(_data, pen_colors[i])
-
-    def _draw_line(self, data, pen_color):
-        self.k_plt.plot(data, pen=pen_color)
 
     def disable_all(self):
         self.re_search_check.setDisabled(True)
