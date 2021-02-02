3899a95219514fccfd9b1a396547e16fa9d7f49f
Jia Liu <proljc@gmail.com>
2021-02-02 21:44:55 +0800

    update cython file list

--- build.py
+++ build.py
@@ -4,8 +4,9 @@ from Cython.Build import cythonize
 setup(
     name='cyber',
     ext_modules=cythonize(
-        ['./apis/k_charts.py', './apis/realtime_price.py',
-         './apis/stock_info.py',
+        ['./apis/code_index.py', './apis/finance.py',
+         './apis/k_charts.py', './apis/realtime_price.py',
+         './apis/statements.py',
          './conf/conf.py', './conf/version.py',
          './db/db.py', './db/models.py', './db/ops.py',
          './pages/about.py', './pages/backtest.py', './pages/choose.py',
@@ -14,8 +15,9 @@ setup(
          './strategies/boll.py', './strategies/common.py',
          './strategies/custom_watch.py', './strategies/dual_line.py',
          './strategies/kdj.py', './strategies/macd.py', './strategies/rsi.py',
-         './strategies/stairs.py', './strategies/turtle.py',
-         './strategies/volume_increase.py', './strategies/wr.py',
+         './strategies/stairs.py', './strategies/triple_golden_cross.py',
+         './strategies/turtle.py', './strategies/volume_increase.py',
+         './strategies/wr.py',
          './utils/candlestick.py', './utils/custom_add_dialog.py',
          './widgets/plots.py'
          ])
