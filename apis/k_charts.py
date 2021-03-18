e47853fe840aac1ca02f0d2b1cd6e08346906f9b
Jia Liu <proljc@gmail.com>
2021-03-18 20:11:48 +0800

    Fix too many data insert

--- apis/k_charts.py
+++ apis/k_charts.py
@@ -171,19 +171,16 @@ class FetchDayK(QThread):
         step = int(total_num / 100)
         i = 0
         j = 0
-        all_data = []
         for code in self.stock_code_list:
             i += 1
             ret, data = fetch_day_line_data(code, self.e_date)
             if ret != 0:
                 self.err_signal.emit(data)
                 return False
-            all_data += data
+            store_day_line_data(data)
             if i % step == 0:
                 j += 1
                 self.sig_fetch_day_k.emit(j)
-
-        store_day_line_data(all_data)
 
         self.sig_fetch_day_k_done.emit()
         bs.logout()
